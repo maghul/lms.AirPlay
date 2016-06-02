f49fe5f1b0da6192b79d2b3baaf7f28bbb969dd0
Magnus Hulthén <magnus@computer.se>
2016-06-02 17:02:49 +0200

    Fixed volume handling
    iDevice communication is handled by the helper app now

--- Squeezebox.pm
+++ Squeezebox.pm
@@ -15,14 +15,17 @@ my $log           = logger('plugin.airplay');
 my $baseUrl       = Plugins::AirPlay::Squeezeplay::getBaseUrl();
 my $center_volume = 52;
 
+my $airplay = {};
+
 sub new {
 }
 
 sub initClient {
         my $client = shift;
 
-        $client{airplay} = { name => $client->name() };
-        $log->debug( "client=" . $client->name() . ", airplay=" . Data::Dump::dump( $client{airplay} ) );
+        if ( !exists $$airplay{ $client->id() } ) {
+                $$airplay{ $client->id() } = { name => $client->name() };
+        }
 }
 
 #sub getinfo {
@@ -75,8 +78,8 @@ sub stop_player {
 sub metaDataProvider {
         my ( $client, $url ) = @_;
 
-        if ( !$client{airplay}->{metadata} ) {
-                $client{airplay}->{metadata} = {
+        if ( !$$airplay{ $client->id() }->{metadata} ) {
+                $$airplay{ $client->id() }->{metadata} = {
                         artist => "",
                         album  => "",
 
@@ -84,8 +87,7 @@ sub metaDataProvider {
                         type    => "AirPlay"
                 };
         }
-        print Data::Dump::dump( $client{airplay}->{metadata} );
-        return $client{airplay}->{metadata};
+        return $$airplay{ $client->id() }->{metadata};
 }
 
 sub dmap_lisitingitem_notification {
@@ -111,7 +113,7 @@ sub dmap_lisitingitem_notification {
                 }
         );
 
-        $client{airplay}->{metadata} = {
+        $$airplay{ $client->id() }->{metadata} = {
                 artist => $$dmap{'daap.songartist'},
                 album  => $$dmap{'daap.songalbum'},
 
@@ -153,12 +155,29 @@ sub volume_notification {
         my $client = shift;
         my $volume = shift;
 
-        #    	my $prev_volume= $client{airplay}->{sb_volume};
-        #    	$client{airplay}->{sb_volume}= $volume;
-        $log->debug( "client=" . $client->name() . ", volume='$volume', airplay=" . Data::Dump::dump( $client{airplay} ) );
+        my $d = $$airplay{ $client->id() };
+        $d->{device_volume} = $volume;
 
         $client->execute( [ "mixer", "volume", $volume ] );
 
+}
+
+sub mixerVolumeQueryCallback {
+        my $request = shift;
+        my $client  = $request->client;
+
+        my $volume = $request->getResult("_volume");
+        my $name   = $client->name();
+        my $dev    = $$airplay{ $client->id() };
+        if ( !$dev->{relative} || ( $dev->{precise} ) ) {
+                $log->debug( "$name: request volume=$volume, device volume=" . $d->{device_volume} );
+                if ( $volume != $d->{device_volume} ) {
+                        setAirPlayDeviceVolume( $client, $volume + 0 );
+                }
+        }
+        else {
+                $log->debug("$name: request volume, device is using relative volume, not sending any data");
+        }
 }
 
 sub mixerVolumeCallback {
@@ -167,15 +186,28 @@ sub mixerVolumeCallback {
 
         return if !defined $client;
 
-        #	$log->debug("getinfo... client=".$client);
-        #	my $info= getinfo($client);
-        $log->debug( "client=" . $client->name() . ", airplay=" . Data::Dump::dump( $client{airplay} ) );
-        if ( $client{airplay}->{relative} ) {
-                setAirPlayDeviceVolume( $client, "relative" );
+        my $dev = $$airplay{ $client->id() };
+        if ( !$dev->{relative} || ( $dev->{precise} ) ) {
+
+                # The volume sent as argument may not be the correct one as it can be the
+                # fixed volume intended for the squeezebox. Ask for the real one instead
+                # and send that to the device.
+		my $request = Slim::Control::Request->new( $client->id(), [ 'mixer', 'volume', '?' ], 1 );
+		$request->callbackParameters( \&mixerVolumeQueryCallback );
+		$request->execute();
         }
-        else {
-                setAirPlayDeviceVolume( $client, $volume + 0 );
+}
+
+sub _setExternalVolumeInfo {
+        my ( $dev, $param ) = @_;
+
+        if ( $param =~ /([a-z]*):([01])/ ) {
+                if ( $dev->{$1} != $2 ) {
+                        $dev->{$1} = $2;
+                        return 1;
+                }
         }
+        return 0;
 }
 
 # TODO: We should get the callback when prefs are changed but that doesn't seem to happen...
@@ -184,26 +216,24 @@ sub externalVolumeInfoCallback {
         $client = $request->client;
 
         if ($client) {
-                my $relative = $request->getParam('_p1');
-                my $precise  = $request->getParam('_p2');
-                $log->debug( "client=" . $client->name() . ", id=" . $client->id() . ", relative=$relative, precise=$precise" );
+                my $dev = $$airplay{ $client->id() };
 
-                if ( defined $relative ) {
+                my $change = 0;
+                $change |= _setExternalVolumeInfo( $dev, $request->getParam('_p1') );
+                $change |= _setExternalVolumeInfo( $dev, $request->getParam('_p2') );
+
+                if ( $change && $dev->{relative} && !( $dev->{precise} ) ) {
                         setAirPlayDeviceVolume( $client, "relative" );
                 }
-
-                $client{airplay}->{relative} = $relative;
-                $client{airplay}->{precise}  = $precise;
-                $log->debug( "client=" . $client->name() . ", id=" . $client->id() . ", relative=$client{airplay}->{relative}, precise=$client{airplay}->{precise}" );
-
-                $log->debug( "client=" . $client->name() . ", airplay=" . Data::Dump::dump( $client{airplay} ) );
         }
 }
 
 sub send_volume_control_state {
         $client = shift;
-        setAirPlayDeviceVolume( $client, ( defined $client{airplay}->{relative} ) ? "relative" : "absolute" );
         $log->debug( $client->name() . ": id=" . $client->id() . ", relative=$$airplay{$client->id()}->{relative}, precise=$$airplay{$client->id()}->{precise}" );
+        if ( defined $$airplay{ $client->id() }->{relative} ) {
+                setAirPlayDeviceVolume( $client, "relative" );
+        }
 }
 
 sub find_client {
