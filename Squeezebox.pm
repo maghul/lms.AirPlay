b87192f0424e64c08c71704c718cbcebad53aaf0
Magnus Hulthén <magnus@computer.se>
2016-06-02 15:29:30 +0200

    Setting metadata for current track

--- Squeezebox.pm
+++ Squeezebox.pm
@@ -46,6 +46,27 @@ sub notification {
         my ($notification) = @_;
 
         $log->warn( "\nNotification\n" . Data::Dump::dump($notification) . "\nNotification\n" );
+
+        my $trackurl = "http://mauree:6111/whatever/audio.pcm";
+        Slim::Music::Info::setRemoteMetadata( $trackurl, { title => $$dmap{'dmap.itemname'}, } );
+        my $itemid = $$dmap{'dmap.persistentid'};
+        my $obj    = Slim::Schema::RemoteTrack->updateOrCreate(
+                $trackurl,
+                {
+                        title  => $$dmap{'dmap.itemname'},
+                        artist => $$dmap{'daap.songartist'},
+                        album  => $$dmap{'daap.songalbum'},
+
+                        #		secs    => $track->{'duration'} / 1000,
+                        coverurl => "airplayimage/whatever/cover.$itemid.jpg",
+                        tracknum => $$dmap{'daap.songtracknumber'},
+                        bitrate  => 44100,
+                        type     => "AirPlay"
+                }
+        );
+
+        $log->debug( "Playing info: " . Data::Dump::dump($obj) );
+
 }
 
 1;
